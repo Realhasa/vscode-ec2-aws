d67e75effc63ceaa529f56a7f2aeac394bcd1dd7
rialhasan <realhasan74@gmail.com>
2026-02-26 23:36:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const pug = require('pug');
+
+var app = express();
+
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.get('/home', (req, res) => {
+    const subjects = ['DevOps', 'CC', 'SPM', 'CD', 'CNS'];
+    res.render('sample.pug', {'sub': subjects})
+})
+
+
+
+
+
+
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server listen at port ${port}`)
+})
