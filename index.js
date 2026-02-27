0298354f89b8242670a2bbea43431dabb4cdba42
rialhasan <realhasan74@gmail.com>
2026-02-27 12:08:20 +0530

    last commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'pug');
 
 
 app.get('/home', (req, res) => {
-    const subjects = ['DevOps', 'CC', 'SPM', 'CD', 'CNS', 'cns lab', 'cc lab'];
+    const subjects = ['DevOps', 'CC', 'SPM', 'CD', 'CNS', 'cns lab', 'cc lab', 'and this Rial from Bangladesh to save the World'];
     res.render('sample.pug', {'sub': subjects})
 })
 
