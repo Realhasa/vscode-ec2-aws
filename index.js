32b2e7e2b3a7f2120cfd383bdb75160b6d170509
rialhasan <realhasan74@gmail.com>
2026-02-27 12:37:16 +0530

    second jjssssj

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'pug');
 
 
 app.get('/home', (req, res) => {
-    const subjects = ['Hello world','DevOps', 'CC', 'SPM', 'CD', 'CNS','prasad', 'cns lab', 'cc lab', 'and this is Rial from Bangladesh to save the World'];
+    const subjects = ['Hello world','DevOps', 'CC', 'SPM', 'CD', 'CNS','prasad', 'cns lab', 'cc lab', 'and this is Rial from Bangladesh to save the World', 'Ameriaca--iran war will be very interesting and soon it will be come ,,, so let"s enjoy'];
     res.render('sample.pug', {'sub': subjects})
 })
 
